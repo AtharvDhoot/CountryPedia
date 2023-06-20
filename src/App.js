cca717c5b1380f04abde375f69191b5376523330
Atharv Dhoot <82392230+AtharvDhoot@users.noreply.github.com>
2023-06-20 23:40:23 +0530

    add: routing setup with react-router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { WorldMapPage } from "./world_map_page/WorldMapPage";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CountryPage } from "./country_page/CountryPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<WorldMapPage />}></Route>
+          <Route path="/:countryIso" element={<CountryPage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
